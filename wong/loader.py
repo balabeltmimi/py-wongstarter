51fefc13bbaddb8969fc0ed3650462015d1322b6
phylynxbelt <thanaporn.jira@odds.team>
2021-05-30 15:24:33 +0700

    build: add flask8

---
+++ wong/loader.py
@@ -0,0 +1,3 @@
+def load_db(filename: str) -> list[]:
+    # load_db should input file name and return book list
+    pass
---
+++ wong/wong.py
@@ -0,0 +1,6 @@
+def main():
+    # TODO: load DB from csv
+    # TODO: query from author, hardest book, date range
+    # TODO: render result from query
+    # Note: flake8 -> should have newline at the end of file
+    pass
